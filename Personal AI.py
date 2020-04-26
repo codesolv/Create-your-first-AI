297f7fae69a6808fd2e8c4ebf23cb320ae29d294
codesolv <64346973+codesolv@users.noreply.github.com>
2020-04-26 13:09:56 +0530

    Update Personal AI.py

--- Personal AI.py
+++ Personal AI.py
@@ -152,6 +152,8 @@ if __name__ == "__main__":
 
         elif 'quit' in query:
             exit(0)
+             
+                #Code By Sourav
 
-
-                                                                                                                                                        #By Sourav
+    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        #By Sourav
